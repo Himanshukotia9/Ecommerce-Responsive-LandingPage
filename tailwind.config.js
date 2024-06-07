4d32291f18335a08ad608f2a02c15b28fd339422
HimanshuKotia9 <himanshukotia9@gmail.com>
2024-06-07 19:47:37 +0530

    basic boilerplate

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        League: "'League Spartan', serif"
+      }
+    },
   },
   plugins: [],
 }
