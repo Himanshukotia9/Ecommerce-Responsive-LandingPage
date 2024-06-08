57fc47875b21bc2e1aa011095b927a469966fe11
HimanshuKotia9 <himanshukotia9@gmail.com>
2024-06-08 18:26:39 +0530

    3 new sections added

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,11 @@ module.exports = {
         'button': "url('/images/button.png')",
         'hero': "url('/images/hero4.png')",
         'banner': "url('images/banner/b2.jpg')",
-        
+        'big-banner-1': "url('images/banner/b17.jpg')",
+        'big-banner-2': "url('images/banner/b10.jpg')",
+        'small-banner-1': "url('images/banner/b7.jpg')",
+        'small-banner-2': "url('images/banner/b4.jpg')",
+        'small-banner-3': "url('images/banner/b18.jpg')",
       }
     },
   },
